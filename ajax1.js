cc6dc4aedbc8a54f1befedb6a8373791fb06e6dc
thecichos <thecichos@gmail.com>
2015-11-19 13:39:06 +0100

    added sort of armors

--- ajax1.js
+++ ajax1.js
@@ -1,14 +1,31 @@
-	var oReq = new XMLHttpRequest();
-	var url = "array.json"
+function compare(a, b) {
+	if (a.Rating < b.Rating) {
+		return -1;
+	}
+	if (a.Rating > b.Rating) {
+		return 1;
+	}
+	return 0;
+}
 
-	function fill(arr) {
-		for (var i = 0; i < arr.length; i++) {
+function fill(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].Rating == document.getElementById('change').value) {
 			var row = "";
 			row = '<tr><td>' + arr[i].Name + '</td><td>' + arr[i].Rating + '</td></tr>';
-			$('#table').prepend(row);
+			$('#table').append(row);
 		}
 	}
+}
+var oReq = new XMLHttpRequest();
+var url = "array.json"
+var ratings = [];
+var newArra = [];
 
+function begin() {
+	if ($("tr").size() != 0) {
+		$("#table").empty();
+	}
 	oReq.onreadystatechange = function() {
 		if (oReq.readyState == 4 && oReq.status == 200) {
 			var myArr = JSON.parse(oReq.responseText);
@@ -17,3 +34,4 @@
 	}
 	oReq.open("GET", url, true);
 	oReq.send();
+}
