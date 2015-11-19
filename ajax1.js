119240df1d68bbc77489b5c0121f537681e63dc7
thecichos <thecichos@gmail.com>
2015-11-19 14:44:18 +0100

    made better search

--- ajax1.js
+++ ajax1.js
@@ -8,12 +8,34 @@ function compare(a, b) {
 	return 0;
 }
 
+function type(category, pattern) {
+	this.category = category;
+	this.pattern = pattern;
+}
+var iron = new type("iron", /iron/i);
+var rope = new type("rope", /rope/i);
+var Daedric = new type("Daedric", /Daedric/i);
+var glass = new type("glass", /glass/i);
+var Dragonplate = new type("Dragonplate", /Dragonplate/i);
+var Dragonscale = new type("Dragonscale", /Dragonscale/i);
+var nord = new type(nord, /nord/i);
+var patterns = [rope, iron, Daedric, glass, Dragonplate, Dragonscale, nord]
+
 function fill(arr) {
+	arr.sort(compare);
+	var x = 0
 	for (var i = 0; i < arr.length; i++) {
 		if (arr[i].Rating == document.getElementById('change').value) {
-			var row = "";
-			row = '<tr><td>' + arr[i].Name + '</td><td>' + arr[i].Rating + '</td></tr>';
-			$('#table').append(row);
+			for (x; x < patterns.length; x++) {
+				if (document.getElementById('type').value == patterns[x].category) {
+					break;
+				}
+			}
+			if (arr[i].Name.search(patterns[x].pattern) > 0) {
+				var row = "";
+				row = '<tr><td>' + arr[i].Name + '</td><td>' + arr[i].Rating + '</td></tr>';
+				$('#table').append(row);
+			}
 		}
 	}
 }
